498bff57ef596dfdb0bf0632131052eae283682f
takemaro <info@takemaro.com>
2022-12-04 10:49:17 +0900

    use SATySFi dev-0.1.0 package system

--- test.sh
+++ test.sh
@@ -2,7 +2,11 @@ if [ "$(satysfi --version)" != "SATySFi version 0.1.0 alpha" ]; then
   echo 'SATySFi version 0.1.0 alpha needed' 1>&2
   exit 1
 fi
-cp src/mathml.satyh-html $HOME/.satysfi/dist/packages/
+satysfi solve .
+mkdir -p $HOME/.satysfi/dist/packages/mathml/mathml.0.0.1/src
+cp satysfi.yaml $HOME/.satysfi/dist/packages/mathml/mathml.0.0.1/
+cp package.satysfi-lock $HOME/.satysfi/dist/packages/mathml/mathml.0.0.1/
+cp src/mathml.satyh-html $HOME/.satysfi/dist/packages/mathml/mathml.0.0.1/src/
 for f in `find tests/*.saty`; do
-  satysfi --text-mode html $f -o "${f%.*}.html"
+  satysfi solve $f && satysfi build --text-mode html $f -o "${f%.*}.html"
 done
